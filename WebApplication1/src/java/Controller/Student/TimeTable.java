e9396e6827deebc086e97b914603f2e856a0e397
phatmvp <phat0972430986@gmail.com>
2024-03-17 23:42:16 +0700

    khoi tao Controller Student TimeTable
    truyen du lieu len file timetable.jsp cua student

--- WebApplication1/src/java/Controller/Student/TimeTable.java
+++ WebApplication1/src/java/Controller/Student/TimeTable.java
@@ -2,22 +2,19 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-
 package Controller.Student;
 
 import Controller.Authentication.BaseRBACController;
-import dal.LecturerDBContext;
 import dal.LessionDBContext;
 import dal.StudentDBContext;
 import dal.TimeSlotDBContext;
+import entity.Account;
 import entity.Lession;
 import entity.Role;
 import entity.Student;
 import entity.TimeSlot;
 import java.io.IOException;
-import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
@@ -28,46 +25,46 @@ import util.DateTimeHelper;
  *
  * @author Admin
  */
-public class TimeTable extends HttpServlet {
+public class TimeTable extends BaseRBACController {
+
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp, Account account, ArrayList<Role> roles) throws ServletException, IOException {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp, Account account, ArrayList<Role> roles) throws ServletException, IOException {
         int lid = Integer.parseInt(req.getParameter("id"));
         String raw_from = req.getParameter("from");
         String raw_to = req.getParameter("to");
         Date today = new Date();
-        java.sql.Date from = null; 
+        java.sql.Date from = null;
         java.sql.Date to = null;
-        
-        if(raw_from ==null)
-        {
+
+        if (raw_from == null) {
             from = DateTimeHelper.convertUtilDateToSqlDate(DateTimeHelper.getWeekStart(today));
-        }
-        else
-        {
+        } else {
             from = java.sql.Date.valueOf(raw_from);
         }
-        
-        if(raw_to == null)
-        {
+
+        if (raw_to == null) {
             to = DateTimeHelper.convertUtilDateToSqlDate(DateTimeHelper.
-                    addDaysToDate(DateTimeHelper.getWeekStart(today),6));
-        }
-        else
-        {
+                    addDaysToDate(DateTimeHelper.getWeekStart(today), 6));
+        } else {
             to = java.sql.Date.valueOf(raw_to);
         }
-        
+
         ArrayList<java.sql.Date> dates = DateTimeHelper.getDatesBetween(from, to);
-        
+
         TimeSlotDBContext slotDB = new TimeSlotDBContext();
         ArrayList<TimeSlot> slots = slotDB.list();
-        
+
         LessionDBContext lessDB = new LessionDBContext();
         ArrayList<Lession> lessions = lessDB.getBy(lid, from, to);
-        
+
         StudentDBContext stud = new StudentDBContext();
         ArrayList<Student> students = stud.list();
-        
+
         req.setAttribute("from", from);
         req.setAttribute("to", to);
         req.setAttribute("slots", slots);
